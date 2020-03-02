ef16cf655fbe5d5d8fa6cfb8aa8fabd3062f475b
Karim Shehadeh <kshehadeh@underarmour.com>
2020-03-02 13:21:14 -0500

    Part of a larger naming convention effort to make type names more consistent.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import {IRouteDefinition, NexusModule, NexusModuleConfig} from "@nexus-switchboard/nexus-extend";
+import {IRouteDefinition, NexusModule, ModuleConfig} from "@nexus-switchboard/nexus-extend";
 import {routes} from "./routes";
 
 export class LivenessModule extends NexusModule {
 
     public name = "liveness";
 
-    public loadRoutes(_config: NexusModuleConfig): IRouteDefinition[] {
+    public loadRoutes(_config: ModuleConfig): IRouteDefinition[] {
         return routes;
     }
 }
